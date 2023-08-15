5750faccd0a301321ad77bd7995765944a2e640a
Ihor <ihor.cherednychenko@nure.ua>
2023-08-15 15:36:15 +0300

    Variables in Java

---
+++ src/Variables.java
@@ -0,0 +1,21 @@
+public class Variables {
+    public static void main(String[] args) {
+        // Целочисленные
+        int myInt = 557;
+        short myShort = 3266;
+        long myLong = 2452345;
+        // Вещественные
+        double myDouble = 55.7;
+        float myFloat = 32.6f;
+        // Characters
+        char c = '3';
+        // Stores text
+        String myString = "Kitty";
+        // Logic
+        boolean b = true;
+        // Primitive data type
+        byte myByte = 43; // -128 to 127
+        // Out something
+        System.out.println(myInt);
+    }
+}
