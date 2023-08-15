19952cb4cdc0ab3b9797a6d2571d65c5d2a3e194
Ihor <ihor.cherednychenko@nure.ua>
2023-08-15 15:50:41 +0300

    Reference data types

---
+++ src/Strings.java
@@ -0,0 +1,12 @@
+public class Strings {
+    public static void main(String[] args) {
+        int x = 5;
+        // Reference data types
+        String s = "Hello";
+        String space = " ";
+        String name = "Kitty";
+        System.out.println(s + space + name);
+        System.out.println("Hello" + " " + "Ihor");
+        System.out.println("My number is: " + x);
+    }
+}
--- src/Variables.java
+++ src/Variables.java
@@ -1,5 +1,6 @@
 public class Variables {
     public static void main(String[] args) {
+        // Primitive data type
         // Целочисленные
         int myInt = 557;
         short myShort = 3266;
@@ -9,11 +10,9 @@ public class Variables {
         float myFloat = 32.6f;
         // Characters
         char c = '3';
-        // Stores text
-        String myString = "Kitty";
         // Logic
         boolean b = true;
-        // Primitive data type
+
         byte myByte = 43; // -128 to 127
         // Out something
         System.out.println(myInt);
