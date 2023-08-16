0f93f8d499e8220a0cb85ee7f5be64eb94c9da22
Ihor <ihor.cherednychenko@nure.ua>
2023-08-16 17:16:42 +0300

    Class Scanner (input data in Java)

---
+++ src/Input.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Input {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Введите ваше Имя: ");
+        String name = scan.nextLine();
+        System.out.print("Введите вашу Фамилию: ");
+        String surName = scan.nextLine();
+        System.out.print("Ваш возрост: ");
+        int age = scan.nextInt();
+        System.out.println("Ваши данные: " + name + " " + surName + " " + age + " year");
+//        System.out.println("Вы ввели: " + s);
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.println("Устроится ли Котик на работу?");
+//        boolean findWork = scanner.nextBoolean();
+//        if (findWork == true) {
+//            System.out.println("We would live together my Kitty ❤️❤️❤️");
+//        } else {
+//            System.out.println("He is trying to find work");
+//        }
+    }
+}
