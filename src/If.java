2fe1b048a4ff604ab8989ad8822caf71b3380c0a
Ihor <ihor.cherednychenko@nure.ua>
2023-08-16 16:40:13 +0300

    The if statement in Java

---
+++ src/If.java
@@ -0,0 +1,12 @@
+public class If {
+    public static void main(String[] args) {
+        boolean findWork = true;
+        if (findWork == true) {
+            System.out.println("We would live together my Kitty ❤️❤️❤️");
+        } else if (findWork == false) {
+            System.out.println("We will not :(");
+        } else {
+            System.out.println("Trying to find work");
+        }
+    }
+}
