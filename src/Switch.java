e7cff3d4c2194d4c212597d2022f3904eb6cd427
Ihor <ihor.cherednychenko@nure.ua>
2023-08-17 20:49:25 +0300

    The switch statement in Java

--- src/Switch.java
+++ src/Switch.java
@@ -11,5 +11,15 @@ public class Switch {
             case "eighteen" -> System.out.println("You finish school");
             default -> System.out.println("Ни одно из предедущих условий не подошло");
         }
+        // The same
+//        if ("zero".equals(age)) {
+//            System.out.println("You born");
+//        } else if ("seven".equals(age)) {
+//            System.out.println("You went to school");
+//        } else if ("eighteen".equals(age)) {
+//            System.out.println("You finish school");
+//        } else {
+//            System.out.println("Ни одно из предедущих условий не подошло");
+//        }
     }
 }
