925adc86de1bf2af53635d15a1f80df80548d8a7
Ihor <ihor.cherednychenko@nure.ua>
2023-08-21 19:09:43 +0300

    Keyword "static" in Java

--- src/Constructors.java
+++ src/Constructors.java
@@ -1,27 +1,22 @@
 public class Constructors {
     public static void main(String[] args) {
-        Human1 human = new Human1();
+        Human1 human1 = new Human1("Bob", 45);
+        Human1 human2 = new Human1("Tomas", 27);
+        Human1 human3 = new Human1("Rob", 55);
+        Human1.printNumberOfPeople();
     }
 }
 
 class Human1 {
     private String name;
     private int age;
-
-    public Human1() {
-        this.name = "Имя по умолчанию";
-        this.age = 0;
-    }
-
-    public Human1(String name) {
-        System.out.println("Привет из второго конструктора!");
-        this.name = name;
-    }
+    private static int countPeople = 0;
 
     public Human1(String name, int age) {
-        System.out.println("Привет из третьего конструктора!");
+//        System.out.println("Привет из третьего конструктора!");
         this.name = name;
         this.age = age;
+        countPeople++;
     }
 
     public void setName(String name) {
@@ -31,4 +26,17 @@ class Human1 {
     public void setAge(int age) {
         this.age = age;
     }
+
+    public static void printNumberOfPeople() {
+        System.out.println("Number of people: " + countPeople);
+    }
 }
+
+//    public Human1() {
+//        this("Bob",12);
+//    }
+//
+//    public Human1(String name) {
+//        System.out.println("Привет из второго конструктора!");
+//        this.name = name;
+//    }
