17bdd955c4d165a270bda42703cfeaaabdb05e28
Ihor <ihor.cherednychenko@nure.ua>
2023-08-22 18:32:24 +0300

    String formatting in Java

--- src/String_Builder.java
+++ src/String_Builder.java
@@ -1,12 +1,14 @@
 public class String_Builder {
     public static void main(String[] args) {
-        StringBuilder sb = new StringBuilder("Hello");
-        sb.append(" my").append(" friend");
-        System.out.println(sb);
-//        String string = "Hello";
-//        String string1 = " my";
-//        String string2 = " friend";
-//        String stringAll = string + string1 + string2;
-//        System.out.println(stringAll);
+        System.out.printf("String %.2f \n", 4.523);
+        System.out.printf("String %.2f \n", 4.52);
+        System.out.printf("String %.2f \n", 4.525131);
+        System.out.printf("String %.2f \n", 4.525131431);
+        System.out.printf("String %10d \n", 525);
+        System.out.printf("String %10d \n", 52);
+        System.out.printf("String %10d \n", 5250);
+        System.out.printf("String %10d \n", 100000);
+        System.out.printf("String %10d \n", 1000000000);
+        // % [10] - ширина строки (- чтобы слева) [.3], количество чисел после запятой
     }
 }
