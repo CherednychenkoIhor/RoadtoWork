f0492cd2738eedb8d0b0d24a24aca8ed1ec3854e
Ihor <ihor.cherednychenko@nure.ua>
2023-08-22 18:18:44 +0300

    StringBuilder in Java

---
+++ src/String_Builder.java
@@ -0,0 +1,12 @@
+public class String_Builder {
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder("Hello");
+        sb.append(" my").append(" friend");
+        System.out.println(sb);
+//        String string = "Hello";
+//        String string1 = " my";
+//        String string2 = " friend";
+//        String stringAll = string + string1 + string2;
+//        System.out.println(stringAll);
+    }
+}
