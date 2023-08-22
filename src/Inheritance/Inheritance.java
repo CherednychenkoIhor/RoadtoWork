2476f814c669d7822d9518839c19ee97c17d0acd
Ihor <ihor.cherednychenko@nure.ua>
2023-08-22 19:10:41 +0300

    Inheritance in Java

---
+++ src/Inheritance/Inheritance.java
@@ -0,0 +1,11 @@
+package Inheritance;
+
+public class Inheritance {
+    public static void main(String[] args) {
+        Dog dog = new Dog();
+        dog.eat();
+        dog.showName();
+        Animal animal = new Animal();
+        animal.eat();
+    }
+}
--- src/ObjectToString.java
+++ src/ObjectToString.java
@@ -1,15 +1,15 @@
 public class ObjectToString {
     public static void main(String[] args) {
-        Human2 human1 = new Human2("Bob", 45);
+        Human human1 = new Human("Bob", 45);
         System.out.println(human1);
     }
 }
 
-class Human2 {
+class Human {
     private String name;
     private int age;
 
-    public Human2(String name, int age) {
+    public Human(String name, int age) {
         this.name = name;
         this.age = age;
     }
