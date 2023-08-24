a632cd6913d29bc5c79c9fd1bc007adbefb217f5
Ihor <ihor.cherednychenko@nure.ua>
2023-08-24 15:14:58 +0300

    Access modifiers public, private, protected and default in Java

--- src/Test.java
+++ src/Test.java
@@ -1,11 +1,17 @@
-import Forest.Something.SomeClass;
-import Forest.*;
-import java.util.*;
+import Package1.Person;
 
-public class Test {
+public class Test extends Person{
+    protected int id;
+
+    public Test() {
+        name = "Tom";
+    }
+
     public static void main(String[] args) {
-        Squirrel squirrel = new Squirrel();
-        Tree tree = new Tree();
-        Scanner scanner = new Scanner(System.in);
+        // public, private, default (Всё что default, доступно в пределах пакета) protected (доступно в пределах
+        // одного пакета, доступны всем подклассам, даже если эти подклассы вне пакета, где эти поля или методы были
+        // декларированы)
+        Person person = new Person();
+//        System.out.println(person.name);
     }
 }
