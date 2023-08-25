4ff13ca7382b0164b705325515e9e700e347d4db
Ihor <ihor.cherednychenko@nure.ua>
2023-08-25 18:19:46 +0300

    Wrapper classes for primitive data types in Java

---
+++ src/WrapperClassesForPrimitiveDT/Test.java
@@ -0,0 +1,12 @@
+package WrapperClassesForPrimitiveDT;
+
+public class Test {
+    public static void main(String[] args) {
+        int x = 1;
+        Integer x2 = 123;
+        int y = x2;
+        
+
+        // Double, Float, Long, Integer, Short, Byte, Character and Boolean
+    }
+}
