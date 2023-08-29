e481e698dd4b3f12ad81249d78b2f33ad9ab9770
Ihor <ihor.cherednychenko@nure.ua>
2023-08-29 19:32:53 +0300

    Array serialization in Java

--- src/Serialization/ReadObject.java
+++ src/Serialization/ReadObject.java
@@ -3,6 +3,7 @@ package Serialization;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
+import java.util.Arrays;
 
 public class ReadObject {
     public static void main(String[] args) {
@@ -10,11 +11,15 @@ public class ReadObject {
             FileInputStream fis = new FileInputStream("people.bin");
             ObjectInputStream ois = new ObjectInputStream(fis);
 
-            Person person1 = (Person) ois.readObject();
-            Person person2 = (Person) ois.readObject();
+            Person[] people = (Person[]) ois.readObject();
+//            int personCount = ois.readInt();
+//            Person[] people = new Person[personCount];
+//
+//            for (int i = 0; i < personCount; i++) {
+//                people[i] = (Person) ois.readObject();
+//            }
 
-            System.out.println(person1);
-            System.out.println(person2);
+            System.out.println(Arrays.toString(people));
 
             ois.close();
         } catch (IOException | ClassNotFoundException e) {
